caafbdc847e4afa5696e7eaea0db574539be3092
CAPACITACION-10 <vivelab@ufpso.edu.co>
2019-10-29 16:05:52 -0500

    ejemplo interfas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,15 +22,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initControl () {
         ingresar = ( Button ) findViewById(R.id.ingresar);
         registrar = ( TextView ) findViewById(R.id.registar);
+
+        ingresar.setOnClickListener(this);
+        registrar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.ingresar:
+                Intent intent = new Intent(v.getContext(), dashboard.class);
+                startActivityForResult(intent, 0);
                 break;
 
             case R.id.registar:
+                Intent intent2 = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent2, 0);
                 break;
         }
 
