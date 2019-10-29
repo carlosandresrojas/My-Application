3908f39135900086c3212891eacc9a6cec02ffa4
CAPACITACION-10 <vivelab@ufpso.edu.co>
2019-10-29 16:29:51 -0500

    ejemplo interfas

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -2,13 +2,42 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button inicio;
+    private  Button activity1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initControl ();
+    }
+
+    private void initControl() {
+        inicio =  ( Button ) findViewById(R.id.ingresar);
+        activity1 = ( Button ) findViewById(R.id.activity1);
+
+        inicio.setOnClickListener(this);
+        activity1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.inicio:
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+                break;
+            case R.id.activity1:
+                Intent intent2 = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent2, 0);
+                break;
+        }
     }
 }
